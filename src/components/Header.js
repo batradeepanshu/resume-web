4279808c3dd320821c397cd97d32a78f6271e14a
Deepanshu <deepanshu123456@bitbucket.org>
2018-03-08 17:40:18 +0530

    sdf

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,12 +2,11 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import '../stylesheet/Header.css'
 
-const HOME_URL=process.env.PUBLIC_URL?("/"+"resume-web"):'/';
  const Header=(props)=>{
    return (
      <div className='header-wrapper clearfix' ref={props.heightRef}>
        <ul>
-         <Link to={HOME_URL}>HOME</Link>
+         <Link to={"/"+process.env.PUBLIC_URL}>HOME</Link>
          <Link to='/skills'>SKILLS</Link>
          <Link to='/'>EXPERIENCE</Link>
          <Link to='/'>PROJECTS</Link>
