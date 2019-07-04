92927e95453acb6726c6110bcc92d9bfcc1bf82c
Deepanshu Batra <deebatra1@publicisgroupe.net>
2019-07-04 15:29:14 +0530

    new change

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,50 +2,14 @@ import React, { Component } from "react";
 import { withRouter } from "react-router-dom";
 import Loader from "./Loader";
 import ScreenshotsModal from "./ScreenShotsModal";
+import {PROJECTS} from "../constants/const";
 import "../stylesheet/Projects.css";
 
 class Projects extends Component {
   constructor() {
     super();
     this.imagesLoaded = 0;
-    this.projects = [
-      {
-        name: "Sears parts direct",
-        imageSrc: process.env.PUBLIC_URL + "/assets/searsproject.jpg",
-        webLink: "www.searspartsdirect.com",
-        summary:
-          "<p>Sears, is an American chain of department stores. It also operates online through various websites one of which is searspartsdirect.com.</p><p> The website deals with all kinds of hardware parts and user can order them through the website.The webite's UI is made using Angular 1</p>",
-        screenshots: [
-          "searsHome",
-          "partSearch",
-          "partDetail",
-          "Cart",
-          "waterFilter"
-        ]
-      },
-      {
-        name: "Quote 2 Order",
-        imageSrc: process.env.PUBLIC_URL + "/assets/q2o.jpg",
-        webLink: "www.searspartsdirect.com",
-        summary: "Q20 is a web application made on Angular 1 made for the telecommucation company TELSTRA. The application is used by local telecommunication providers to request for cabling and other services from Telstra. The website lets the users(local Service Providers) to enter all the details related to the cabling including distance ,start-point,end-point ,request a visit by telstra and various other technical details.",
-        screenshots: [
-
-        ]
-      },
-      {
-        name: "Embibe",
-        imageSrc: process.env.PUBLIC_URL + "/assets/embibeproject.jpg",
-        webLink: "www.embibe.com/landing",
-        summary: "Embibe is an Ed-Tech startup which uses AI(Artificial Intelligence) to provide Personalized Education Packs for students preparing for various competetive exams.<p>The application's Front-End is made using React with Redux </p>",
-        screenshots: [
-          "choosemission",
-          "embibe_home_page",
-          "learn",
-          "searchResults",
-          "searchResults2"
-        ]
-      }
-    ];
+    this.projects = PROJECTS;
     this.state = {
       selectedProject: this.projects[2],
       loading: true
---
+++ src/constants/const.js
@@ -0,0 +1,51 @@
+export const PROJECTS = [
+  {
+    name: "Sears parts direct",
+    imageSrc: process.env.PUBLIC_URL + "/assets/searsproject.jpg",
+    webLink: "www.searspartsdirect.com",
+    summary:
+      "<p>Sears, is an American chain of department stores. It also operates online through various websites one of which is searspartsdirect.com.</p><p> The website deals with all kinds of hardware parts and user can order them through the website.The webite's UI is made using Angular 1</p>",
+    screenshots: [
+      "searsHome",
+      "partSearch",
+      "partDetail",
+      "Cart",
+      "waterFilter"
+    ]
+  },
+  {
+    name: "Quote 2 Order",
+    imageSrc: process.env.PUBLIC_URL + "/assets/q2o.jpg",
+    webLink: "www.searspartsdirect.com",
+    summary: "Q20 is a web application made on Angular 1 made for the telecommucation company TELSTRA. The application is used by local telecommunication providers to request for cabling and other services from Telstra. The website lets the users(local Service Providers) to enter all the details related to the cabling including distance ,start-point,end-point ,request a visit by telstra and various other technical details.",
+    screenshots: [
+
+    ]
+  },
+  {
+    name: "Embibe",
+    imageSrc: process.env.PUBLIC_URL + "/assets/embibeproject.jpg",
+    webLink: "www.embibe.com/landing",
+    summary: "Embibe is an Ed-Tech startup which uses AI(Artificial Intelligence) to provide Personalized Education Packs for students preparing for various competetive exams.<p>The application's Front-End is made using React with Redux </p>",
+    screenshots: [
+      "choosemission",
+      "embibe_home_page",
+      "learn",
+      "searchResults",
+      "searchResults2"
+    ]
+  },
+  {
+    name: "Sapient Consulting",
+    imageSrc: process.env.PUBLIC_URL + "/assets/embibeproject.jpg",
+    webLink: "www.embibe.com/landing",
+    summary: "Finance Project",
+    screenshots: [
+      "choosemission",
+      "embibe_home_page",
+      "learn",
+      "searchResults",
+      "searchResults2"
+    ]
+  }
+];
